3ef40a3615e9703486ad6aca7b8432b520951d11
Ravi Kumar <ravibansal130201@gmail.com>
2025-09-14 17:16:29 +0530

    lexical environment details added in Javascript

--- 1 Front End/4 Java Script/script.js
+++
@@ -1,9 +0,0 @@
-// alert("hello !");
-// prompt("What is your name ?")
-// console.log(prompt("What is your name ?"));
-console.log("What is your name ?");
-console.log("Hello !");
-
-let firstName = prompt("What is your name !");
-console.log("Hi, " + firstName + "!");
-console.log("How are you doing, " + firstName + "?");
