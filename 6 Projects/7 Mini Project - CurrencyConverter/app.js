ae64d7241dc735159ecb69b5aab271cbd4c9cd80
Ravi Kumar <ravibansal130201@gmail.com>
2025-11-19 16:43:33 +0530

    change in project names

---
+++ 6 Projects/7 Mini Project - CurrencyConverter/app.js
@@ -0,0 +1,19 @@
+const BASE_URL = "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies";
+
+const dropdowns = document.querySelectorAll(".dropdown select");
+// const btn = document.querySelector();
+
+for (let select of dropdowns) {
+    for (let currCode in countryList) {
+        let newOption = document.createElement("option");
+        newOption.innerText = currCode;
+        newOption.value = currCode;
+        select.append(newOption);
+
+        if (select.name === "from" && currCode === "USD") {
+            newOption.selected = true;
+        } else if (select.name === "to" && currCode === "INR") {
+            newOption.selected = true;
+        }
+    }
+}
